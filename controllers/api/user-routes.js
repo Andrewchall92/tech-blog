a1f89f29d80ff6426917f5378c53c9052e17f479
Andrew Hall <andrewchall92@gmail.com>
2023-11-06 15:51:52 -0800

    I wrote the 'home-page' routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    try {
+        User.findAll({})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
